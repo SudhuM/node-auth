15c268a04cbaf77cc737798686fc3fd47e740135
SudhuM <sudharshan134@gmail.com>
2020-10-26 20:35:18 +0530

    Added password Confirmation Middleware

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 
 const authSchema = Joi.object({
-	name     : Joi.string().required().min(5),
-	email    : Joi.string().email().required().lowercase(),
-	password : Joi.string().required().min(8)
+	name            : Joi.string().required().min(5).trim(),
+	email           : Joi.string().email().required().lowercase().trim(),
+	password        : Joi.string().required().min(8),
+	confirmPassword : Joi.string().required()
 });
 
 module.exports.validationSchema = {
--- models/user.js
+++ models/user.js
@@ -1,22 +1,42 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
-const userSchema = mongoose.Schema({
-	name     : {
-		type      : String,
-		required  : true,
-		minlength : 5
+const userSchema = mongoose.Schema(
+	{
+		name            : {
+			type      : String,
+			required  : true,
+			minlength : 5
+		},
+		email           : {
+			type      : String,
+			required  : true,
+			unique    : true,
+			lowercase : true
+		},
+		password        : {
+			type      : String,
+			required  : true,
+			minlength : 8
+		},
+		// a temp field which will not be stored
+		confirmPassword : {
+			type : String
+		}
 	},
-	email    : {
-		type      : String,
-		required  : true,
-		unique    : true,
-		lowercase : true
-	},
-	password : {
-		type      : String,
-		required  : true,
-		minlength : 8
+	{ timestamps: true }
+);
+
+userSchema.pre('save', function(next) {
+	if (this.isNew || this.isModified('password')) {
+		if (!validator.equals(this.password, this.confirmPassword)) {
+			const error = new Error('Passwords did not match');
+			error.status = 422;
+			return next(error);
+		}
+		this.confirmPassword = undefined;
+		next();
 	}
 });
 
@@ -30,6 +50,10 @@ userSchema.pre('save', async function(next) {
 		next();
 	}
 	next();
+});
+
+userSchema.post('save', function(err, doc, next) {
+	next(err);
 });
 
 const User = mongoose.model('user', userSchema);
